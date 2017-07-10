81798b70985388e4f26c5a8fe3942343d467382e
Makii99 <Makii99@users.noreply.github.com>
2017-07-10 17:24:35 -0500

    Add files via upload

--- Text-Based-Shooter-Alpha.bat
+++ Text-Based-Shooter-Alpha.bat
@@ -1,10 +1,30 @@
 @echo off
 TITLE ---=====Shooter Alpha=====---
 
+:::
+::: ________  ___  ___  ________  ________  _________  _______   ________     
+:::|\   ____\|\  \|\  \|\   __  \|\   __  \|\___   ___\\  ___ \ |\   __  \    
+:::\ \  \___|\ \  \\\  \ \  \|\  \ \  \|\  \|___ \  \_\ \   __/|\ \  \|\  \   
+::: \ \_____  \ \   __  \ \  \\\  \ \  \\\  \   \ \  \ \ \  \_|/_\ \   _  _\  
+:::  \|____|\  \ \  \ \  \ \  \\\  \ \  \\\  \   \ \  \ \ \  \_|\ \ \  \\  \| 
+:::    ____\_\  \ \__\ \__\ \_______\ \_______\   \ \__\ \ \_______\ \__\\ _\ 
+:::   |\_________\|__|\|__|\|_______|\|_______|    \|__|  \|_______|\|__|\|__|
+:::   \|_________|                                                            
+:::		   __          __  ___     __    _ _ 
+:::		  / /  __ __  /  |/  /__ _/ /__ (_|_)
+:::		 / _ \/ // / / /|_/ / _ `/  '_// / / 
+:::		/_.__/\_, / /_/  /_/\_,_/_/\_\/_/_/  And Mike
+:::		     /___/                          
+:::
+:::
+:::                         
+
 :Notice
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo This game is licensed under GPLv3. Do you agree with this license?
 set /p input100=y/n:
+
 if %input100% equ Y goto startup
 if %input100% equ y goto startup
 if %input100% equ N exit
@@ -13,6 +33,7 @@ goto notice
 
 :startup
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------=========Shooter Alpha=========-----------
 echo Welcome to Shooter Text Alpha, Would you like to?
 echo.
@@ -27,14 +48,15 @@ if %input0% equ Play goto Play
 if %input0% equ play goto Play
 if %input0% equ Info goto Info
 if %input0% equ info goto Info
-if %input0% equ Quit exit
-if %input0% equ quit exit
+if %input0% equ Quit goto WF6
+if %input0% equ quit goto WF6
 
 goto startup
 
 
 :Info
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------======================================================------
 echo Here is some Info about this game:
 echo.
@@ -57,6 +79,7 @@ goto Info
 
 :Play
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo You see a Stage Selection in front of you:
 echo there are 3 Stages, Each with a new theme,
 echo a new layout and a new boss! Pick one!
@@ -82,6 +105,7 @@ goto Play
 
 :WF1
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------=================================------
 echo You arrive at Waterfalls, Do you decide to...
 echo.
@@ -103,6 +127,7 @@ goto WF1
 
 :GameOver1WF1
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------================================------
 echo You walk around the Waterfalls, But you trip
 echo Over a little rock, and into the Waterfalls...
@@ -122,6 +147,7 @@ goto GameOver1WF1
 
 :GameOver2WF1
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------===============================------
 echo You jump in the Waterfalls, Little did you know
 echo the water was Poisenious...
@@ -141,6 +167,7 @@ goto GameOver1WF1
 
 :WF2
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------=============================------
 echo You sit on a big rock, Turns out that
 echo big rock was aucually a Button!
@@ -162,6 +189,7 @@ if %InputWF2% equ c goto GameOver2WF2
 
 :GameOver1WF2
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------===========================------
 echo You keep sitting on the rock,
 echo But some Enemies approach You!
@@ -182,6 +210,7 @@ goto GameOver1WF2
 
 :GameOver2WF2
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------================================------
 echo You walk around the Waterfalls, But you trip
 echo Over a little rock, and into the Waterfalls...
@@ -201,6 +230,7 @@ goto GameOver2WF2
 
 :WF3
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------===========================------
 echo You go inside the cave, There are
 echo Enemies waiting for you, One of them
@@ -225,6 +255,7 @@ goto WF3
 
 :GameOver1WF3
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------===========================------
 echo You jump over the Bullet and the Enemie,
 echo However, There is another bullet and
@@ -245,6 +276,7 @@ goto GameOver1WF3
 
 :GameOver2WF3
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------===========================------
 echo You roll into the Enemie, You take a hit
 echo               GAME OVER!
@@ -263,6 +295,7 @@ goto GameOver2WF3
 
 :WF4
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------===========================------
 echo You shoot the enemie, It dies, but
 echo the bullet is still after you...
@@ -282,6 +315,7 @@ goto WF4
 
 :WF5
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------===========================------
 echo You Dodged the bullet, There is a chance
 echo to strike the Enemie... Do you...
@@ -303,6 +337,7 @@ goto WF5
 
 :GameOverWF5
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------===========================------
 echo You charge up your beam, but you get shot
 echo with a Bullet...
@@ -322,6 +357,7 @@ goto GameOverWF5
 
 :GameOver2WF5
 cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo ------===========================------
 echo You attempt to shoot the Enemie and kill
 echo it, but before it died it shot a Bullets
@@ -339,6 +375,83 @@ if %InputGO2WF5% equ n goto startup
 if %InputGO2WF5% equ N goto startup
 
 goto GameOver2WF5
+
+:WF6
+cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+echo ------===========================------
+echo You ignore the Enemie and move on,
+echo you approach a wall of lasers and a hole,
+echo do you...
+echo.
+echo A) Attempt to go down the hole
+echo B) Jump over the hole and look for secrets
+echo C) Camp outside the Entrance
+echo ------===========================------
+set /p InputWF6=Enter:
+
+if %InputWF6% equ A goto WF7LASER1
+if %InputWF6% equ a goto WF7LASER1
+if %InputWF6% equ B goto GameOver1WF6
+if %InputWF6% equ b goto GameOver1WF6
+if %InputWF6% equ C goto GameOver2WF6
+if %InputWF6% equ c goto GameOver2WF6
+
+goto WF6
+
+:GameOver1WF6
+cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+echo ------===========================------
+echo you jump over the hole to look for secrets
+echo ...And then you get blasted by a laser
+echo              GAME OVER!
+echo.
+echo.
+echo Continue? (y/n)
+set /p InputGOWF6=Continue?:
+
+if %InputGOWF6% equ Y goto WF6
+if %InputGOWF6% equ y goto WF6
+if %InputGOWF6% equ N goto startup
+if %InputGOWF6% equ n goto startup
+
+goto GameOver1WF6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
